6e5f913a25b5bf5877154036ab4a6b85a40397d6
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-11-20 23:37:02 +0900

    2022 11 20 - id 받아오기, mustache header, body, footer 분리

--- src/main/java/bbs1120/controller/MustacheController.java
+++ src/main/java/bbs1120/controller/MustacheController.java
@@ -3,13 +3,15 @@ package bbs1120.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MustacheController {
 
-    @GetMapping("/hi")
-    public String hello(Model model) {
+    @GetMapping("/hi/{id}")
+    public String hello(@PathVariable String id, Model model) {
         model.addAttribute("username", "jimandy");
+        model.addAttribute("id", id);
         return "greetings";
     }
 }
